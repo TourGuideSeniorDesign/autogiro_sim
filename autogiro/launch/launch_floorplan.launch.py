7cb309bd35d1e1f8b059b680e5136c9f09587815
benjieiy <hillba@lafayette.edu>
2026-04-05 18:35:00 -0400

    robot model fixed, lidar orientation corrected. Need to work on sync, no /scan feed

--- autogiro/launch/launch_floorplan.launch.py
+++ autogiro/launch/launch_floorplan.launch.py
@@ -90,6 +90,7 @@ def generate_launch_description():
             ('scan', '/scan')
         ],
         parameters=[{
+            # 'use_sim_time': True,
             'target_frame': 'laser_frame',
             'transform_tolerance': 0.01,
             'min_height': 0.1,  
